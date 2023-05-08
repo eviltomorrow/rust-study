b956a861f0a9875b7021f2a73c44d3d0d9d2db07
shepard <eviltomorrow@163.com>
2023-05-09 07:02:49 +0800

    dev

--- chapter09/src/main.rs
+++ chapter09/src/main.rs
@@ -1,6 +1,22 @@
+use std::fs::File;
+
 fn main() {
-    let v = vec![1, 2, 3, 4];
-    for data in v {
-        print!("{data}",);
+    /*
+     * rust 错误分为两大类：可恢复的（recoverable）和不可恢复的（unrecoverable）
+     * 1、不可恢复错误
+     *       显示调用 panic! 宏
+     *       访问数组下标越界这种运行时错误
+     */
+    // panic!("there is one panic");
+
+    let v = vec![1, 2, 3];
+    let a3 = v[2];
+    // let a99 = v[99];
+    println!("{a3}");
+
+    let greeting_file_result = File::open("hello.txt");
+    match greeting_file_result {
+        Ok(_) => println!("ok"),
+        Err(err) => println!("{:#?}", err),
     }
 }
