40ed88de050dd5e08eb793081e8971c29903d5af
liarsa.localdomain <shepard@liarsa.localdomain>
2024-12-05 19:46:08 +0800

    dev

--- book03/src/main.rs
+++ book03/src/main.rs
@@ -27,9 +27,179 @@ fn main() {
 
     let mut i: i8 = 1;
     loop {
+        if i >= 100 {
+            break;
+        }
         i = i.checked_mul(10).expect("overflow i");
-        println!("{}", i);
     }
+
+    let c = 10e2;
+    println!("{}", c);
+    println!("{}", f32::MAX);
+    println!("{}", std::f32::consts::PI);
+
+    assert_eq!(true as i32, 1);
+    assert_eq!('*' as i8, 42);
+
+    let c = std::char::from_u32(42);
+    if let Some(val) = c {
+        println!("{}", val);
+    }
+
+    let text = "I see the eigenvalue in thine eye";
+    let (head, tail) = text.split_at(21);
+    assert_eq!(head, "I see the eigenvalue ");
+    assert_eq!(tail, "in thine eye");
+
+    let temp = text.split_at(21);
+    let head = temp.0;
+    let tail = temp.1;
+    assert_eq!(head, "I see the eigenvalue ");
+    assert_eq!(tail, "in thine eye");
+
+    let mut x = 10;
+    let mut y = 20;
+    swap::<i8>(&mut x, &mut y);
+
+    let c = ("H",);
+    println!("{:?}", c);
+
+    let t = 10;
+    let t_ref = &t;
+    println!("{}, {}", t_ref, *t_ref);
+
+    let t = (12, "eggs");
+    let b = Box::new(t);
+    println!("{:?}", b);
+
+    let lazy_caterer: [u32; 6] = [1, 2, 4, 7, 11, 16];
+    let taxonomy = ["Animalia", "Arthropoda", "Insecta"];
+
+    assert_eq!(lazy_caterer[3], 7);
+    assert_eq!(taxonomy.len(), 3);
+
+    let mut sieve: [bool; 10000] = [true; 10000];
+    for i in 2..100 {
+        if sieve[i] {
+            let mut j = i * i;
+            while j < sieve.len() {
+                sieve[j] = false;
+                j += i;
+            }
+        }
+    }
+
+    assert!(sieve[211]);
+    assert!(!sieve[9876]);
+
+    let c: [u8; 3] = [0; 3];
+    println!("{:?}", c);
+
+    let mut chaos = [3, 5, 4, 1, 2];
+    chaos.sort();
+    println!("{:?}", chaos);
+
+    let mut primes = vec![2, 3, 5, 7];
+    assert_eq!(primes.iter().product::<i32>(), 210);
+    primes.push(11);
+    primes.push(13);
+    assert_eq!(primes.iter().product::<i32>(), 30030);
+
+    let c = new_pixel_buffer(2, 2);
+    println!("{:?}", c);
+
+    let mut pal = Vec::new();
+    pal.push("step");
+    pal.push("on");
+    pal.push("no");
+    pal.push("pets");
+    assert_eq!(pal, vec!["step", "on", "no", "pets"]);
+
+    let v: Vec<i32> = (0..5).collect();
+    assert_eq!(v, [0, 1, 2, 3, 4]);
+
+    let mut palindrome = vec!["A", "B", "C", "D"];
+    palindrome.reverse();
+    println!("{:?}", palindrome);
+
+    let v = Vec::<i32>::with_capacity(2);
+    assert_eq!(v.len(), 0);
+    assert_eq!(v.capacity(), 2);
+
+    let mut v = Vec::with_capacity(2);
+    v.push(1);
+    v.push(2);
+    assert_eq!(v.len(), 2);
+    assert_eq!(v.capacity(), 2);
+
+    v.push(3);
+    assert_eq!(v.capacity(), 4);
+
+    let mut v = vec![10, 20, 30, 40, 50];
+    v.insert(0, 0);
+    println!("{:?}", v);
+
+    v.remove(2);
+    println!("{:?}", v);
+
+    let v = v.pop();
+    if let Some(c) = v {
+        println!("{}", c);
+    }
+
+    let v: Vec<f64> = vec![0.0, 0.707, 1.0, 0.707];
+    let a: [f64; 4] = [0.0, 0.707, 1.0, 0.707];
+
+    let sv: &[f64] = &v;
+    let sa: &[f64] = &a;
+    println!("{}", sv == sa);
+
+    print(&sv[0..2]);
+
+    let speec = "\"Ouch!\" said the well.\n";
+    println!("{}", speec);
+
+    println!(
+        "In the room the women come and go,
+        Singing of Mount Abora"
+    );
+
+    println!(
+        r"In the room the women come and go, \
+        Singing of Mount Abora"
+    );
+
+    println!(r###"In the "room" the women come and \go."###);
+
+    let method = b"GET";
+    println!("{:?}", method);
+
+    let method = "GET";
+    assert_eq!(method.as_bytes(), b"GET");
+
+    let noodles = "noodles".to_string();
+    let oodles = &noodles[1..];
+    println!("{}", oodles);
+
+    let s = String::from("成功");
+    println!("{}", &s[3..]);
+
+    println!("{}", s.chars().count());
+}
+
+fn print(n: &[f64]) {
+    for e in n {
+        println!("{}", e);
+    }
+}
+
+fn new_pixel_buffer(rows: usize, cols: usize) -> Vec<u8> {
+    vec![0; rows * cols]
+}
+
+fn swap<T>(x: &mut T, y: &mut T) -> () {
+    _ = x;
+    _ = y;
 }
 
 fn use_i32(c: i32) {
--- book04/src/main.rs
+++ book04/src/main.rs
@@ -1,75 +1,82 @@
-use std::rc::Rc;
-
 fn main() {
-    println!("Hello, world!");
-
     print_padovan();
 
-    {
-        let point = Box::new((0.625, 0.5));
-        let label = format!("{:?}", point);
-        println!("{}", label);
+    let point = Box::new((0.625, 0.5));
+    let label = format!("{:?}", point);
+    assert_eq!(label, "(0.625, 0.5)");
+
+    struct Person {
+        name: String,
+        birth: i32,
     }
 
-    let v = vec![String::from("A")];
-    let x = &v[0];
+    let mut composers = Vec::new();
+    composers.push(Person {
+        name: String::from("Palestrina"),
+        birth: 1525,
+    });
+    composers.push(Person {
+        name: "Dowland".to_string(),
+        birth: 1563,
+    });
+    composers.push(Person {
+        name: "Lully".to_string(),
+        birth: 1632,
+    });
+
+    for p in composers {
+        println!("{}, {}", p.name, p.birth);
+    }
+
+    let s = vec!["udon".to_string(), "ramen".to_string(), "soba".to_string()];
+    let t = s;
+    let u = t;
+    println!("{:?}", u);
+
+    let x = vec![10, 20, 30];
+    let flag = ok();
+    if flag {
+        move_vec(&x);
+    } else {
+        borrow_vec(&x);
+    }
+    println!("{:?}", x);
+
+    let mut x = Vec::<String>::new();
+    x.push("AA".to_string());
+    x.push("BB".to_string());
+    x.push("CC".to_string());
+
+    for v in x {
+        println!("{:?}", v);
+    }
+    // println!("{:?}", x);
+    //
+    move_string(String::from("Hello"));
+}
+
+fn move_vec(x: &Vec<i32>) {
+    println!("{:?}", x);
+}
+
+fn borrow_vec(x: &Vec<i32>) {
+    println!("{:?}", x);
+}
+
+fn move_string(x: String) {
     println!("{}", x);
-
-    let mut composers = Vec::<Person>::new();
-    composers.push(Person {
-        name: Some("Liarsa".to_string()),
-    });
-
-    println!("{:?}", composers);
-
-    let first_name_option = std::mem::replace(&mut composers[0].name, None);
-    match first_name_option {
-        Some(data) => println!("{}", data),
-        None => println!("None"),
-    }
-    println!("{:?}", composers);
-
-    composers.push(Person {
-        name: Some("Liarsa".to_string()),
-    });
-    let first_name_option = composers[1].name.take();
-    match first_name_option {
-        Some(data) => println!("{}", data),
-        None => println!("None"),
-    }
-    println!("{:?}", composers);
-
-    let a = Animal { age: 10 };
-    println!("{:?}", a);
-    print_animal(a);
-    println!("{:?}", a);
-
-    let s = Rc::new(String::from("Hello world"));
-    println!("{}", s);
-    let t = s.clone();
-
-    println!("{}, {}", s, t);
 }
 
-fn print_animal(a: Animal) {
-    println!("{:?}, {}", a, a.age);
-}
-
-#[derive(Debug, Clone, Copy)]
-struct Animal {
-    age: u32,
-}
-
-#[derive(Debug)]
-struct Person {
-    name: Option<String>,
+fn ok() -> bool {
+    false
 }
 
 fn print_padovan() {
     let mut padovan = vec![1, 1, 1];
+
     for i in 3..10 {
         let next = padovan[i - 3] + padovan[i - 2];
         padovan.push(next);
     }
-    println!("P(1..10) = {:?}", padovan);
+    println!("{:?}", padovan);
 }
