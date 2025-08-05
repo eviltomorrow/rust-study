e313aaa473566d7a29545a31315b1b81b870d3cf
liarsa.me <shepard@liarsa.me>
2025-08-05 19:34:02 +0800

    dev

--- book07/src/main.rs
+++ book07/src/main.rs
@@ -1,15 +1,10 @@
-use std::{thread, time};
-
 fn main() {
     println!("Hello, world!");
 
-    let handler = thread::spawn(|| {
-        let c = [0_i32; 2];
-        for i in 0..100 {
-            let c = c[i];
-            println!("{}", c);
-            thread::sleep(time::Duration::from_secs(1));
-        }
-    });
-    handler.join().expect("Boom!");
+    let mut x = 10;
+    loop {
+        let d = 100 / x;
+        println!("{}", d);
+        x -= 1;
+    }
 }
