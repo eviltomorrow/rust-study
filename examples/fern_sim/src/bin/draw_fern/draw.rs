53131cfdd925d68a9ac6372001540108fcbb00fc
liarsa.me <shepard@liarsa.me>
2026-03-21 14:16:49 +0800

    dev

--- examples/fern_sim/src/bin/draw_fern/draw.rs
+++ examples/fern_sim/src/bin/draw_fern/draw.rs
@@ -1,3 +1,13 @@
+use fern_sim::Fern;
+use fern_sim::run_simulation;
+
 pub fn display_fern_sim() {
     println!("P{}", r"c");
+
+    let mut fern = Fern {
+        size: 1.0,
+        growth_rate: 0.001,
+    };
+    run_simulation(&mut fern, 1000);
+    println!("final fern size: {}", fern.size);
 }
