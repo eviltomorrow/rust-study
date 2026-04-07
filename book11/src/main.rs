53a0417c23b86fbac444fbca89463f0cd38e3e44
liarsa.me <shepard@liarsa.me>
2026-04-07 16:42:06 +0800

    dev

--- book11/src/main.rs
+++ book11/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::Write};
+use std::{fmt::Debug, fs::File, hash::Hash, io::Write};
 
 type GenericError = Box<dyn std::error::Error + Send + Sync + 'static>;
 type GenericResult<T> = Result<T, GenericError>;
@@ -13,6 +13,11 @@ fn main() -> GenericResult<()> {
     say_hello2(&mut bytes)?;
     println!("{:?}", String::from_utf8(bytes));
 
+    let a: [f64; 3] = [0., 0., 0.];
+    let b = [1f64; 3];
+    let sum = dot_product(a, b);
+    println!("{:?}", sum);
+
     Ok(())
 }
 
@@ -24,4 +29,75 @@ fn say_hello1(out: &mut dyn Write) -> std::io::Result<()> {
 fn say_hello2<T: Write>(out: &mut T) -> std::io::Result<()> {
     out.write_all(br"hello world\n")?;
     out.flush()
+}
+
+fn dot_product<const N: usize>(a: [f64; N], b: [f64; N]) -> f64 {
+    let mut sum = 0.;
+    for i in 0..N {
+        sum += a[i] + b[i];
+    }
+    sum
+}
+
+#[allow(dead_code)]
+fn top_ten<T: Debug>(_values: &Vec<T>) {}
+
+#[allow(dead_code)]
+fn top_ten_1<T: Debug + Hash + Eq>(_values: &Vec<T>) {}
+
+#[allow(dead_code)]
+fn run_query<M: Mapper + Serialize, R: Reducer + Serialize>(_data_1: M, _data_2: R) {}
+
+#[allow(dead_code)]
+fn run_query_1<M, R>()
+where
+    M: Mapper + Serialize,
+    R: Reducer + Serialize,
+{
+}
+
+#[allow(dead_code)]
+trait Mapper {}
+#[allow(dead_code)]
+trait Reducer {}
+#[allow(dead_code)]
+trait Serialize {}
+
+#[allow(dead_code)]
+fn nearest<'t, 'c, P>(_target: &'t P, candidates: &'c [P]) -> &'c P
+where
+    P: Debug,
+{
+    &candidates[0]
+}
+
+#[allow(dead_code)]
+enum C<T>
+where
+    T: Debug,
+{
+    NUM3(T),
+}
+
+#[allow(dead_code)]
+trait Vegetable {}
+
+#[allow(dead_code)]
+struct Salad<V: Vegetable> {
+    veggies: Vec<V>,
+}
+
+#[allow(dead_code)]
+struct Salad2<'a, V: Vegetable> {
+    veggies: Vec<&'a V>,
+}
+
+#[allow(dead_code)]
+struct Salad3<'a> {
+    veggies: Vec<&'a dyn Vegetable>,
+}
+
+#[allow(dead_code)]
+struct Salad4 {
+    veggies: Vec<Box<dyn Vegetable>>,
 }
